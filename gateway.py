b4106e61fddad01504a699c5ffee3a324b482d0f
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-11-08 15:23:33 +0100

    added frontend part + service

--- gateway.py
+++ gateway.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "http://localhost:4200"}})
+
+
 
 MODEL_ENDPOINTS = {
     "svm": "http://localhost:5001/predict",  
